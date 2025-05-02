d93370b8919875d650f88501823de1a6aa1d64b0
Anton Knysh <anton.knysh@andevart.com>
2025-05-02 11:45:01 +0300

    expanded the "user-dto"

--- src/dtos/user-dto.js
+++ src/dtos/user-dto.js
@@ -3,11 +3,15 @@ module.exports = class UserDto {
   name;
   email;
   avatarUrl;
+  followers;
+  following;
 
   constructor(model) {
     this.id = model.id;
     this.name = model.name;
     this.email = model.email;
     this.avatarUrl = model.avatarUrl;
+    this.followers = model.followers;
+    this.following = model.following;
   }
 }
