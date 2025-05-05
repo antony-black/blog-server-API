74998eb74df34ec701344c678f7502c2a4e36be3
Anton Knysh <anton.knysh@andevart.com>
2025-05-05 07:36:55 +0300

    added all routes to consts

---
+++ src/constants/routes-paths/index.js
@@ -0,0 +1,10 @@
+const routesPaths = {
+  uploads: "/uploads",
+  users: "/api/users",
+  posts: "/api/posts",
+  comments: "/api/comments",
+  likes: "/api/likes",
+  followers: "/api/followers",
+};
+
+module.exports = routesPaths;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const fs = require("fs");
 
+const routesPaths = require("./constants/routes-paths/index");
 const { usersRouter, postsRouter, commentsRouter, likesRouter, followsRouter } = require("./routes");
 const errorMiddleWare = require("./middlewares/error-middleware");
 
@@ -15,17 +17,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/uploads", express.static("src/uploads"));
-app.use("/api/users", usersRouter);
-app.use("/api/posts", postsRouter);
-app.use("/api/comments", commentsRouter);
-app.use("/api/likes", likesRouter);
-app.use("/api/followers", followsRouter);
+app.use(routesPaths.uploads, express.static("src/uploads"));
+app.use(routesPaths.users, usersRouter);
+app.use(routesPaths.posts, postsRouter);
+app.use(routesPaths.comments, commentsRouter);
+app.use(routesPaths.likesRouter, likesRouter);
+app.use(routesPaths.followers, followsRouter);
 app.use(errorMiddleWare);
 
-if (!fs.existsSync("src/uploads")) {
-  fs.mkdirSync("src/uploads");
+// TODO!!: assign all strings to const
+// TODO!!: create new DTOs for posts, comments, likes etc.? or let it be as is
+const uploadsPath = path.join(__dirname, "src", "uploads");
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath);
 }
+// if (!fs.existsSync("src/uploads")) {
+//   fs.mkdirSync("src/uploads");
+// }
 
 const start = () => {
   try {
